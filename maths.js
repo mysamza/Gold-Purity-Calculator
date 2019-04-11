b75dd3c2890e6ab6ae19422a9d705edc51981738
Mesam Zahid <mesamabbas@hotmail.com>
2019-04-11 19:32:40 +0400

    Create math,js

---
+++ maths.js
@@ -0,0 +1,20 @@
+// Add copper -  so if big to small.
+
+var result = 0;
+var weightinhand = 10;
+var purityinhand = 916;
+var puritywanted = 730;
+
+result = (((weightinhand*purityinhand/1000)/puritywanted)*1000)-weightinhand;
+console.log(result);
+
+// Add pure gold - so if small to big
+
+
+havePromille = 1000-havePromille
+		onskePromille = 1000-onskePromille
+		resultatValue=(((vaegtValue*havePromille/1000)/onskePromille)*1000)-vaegtValue
+		onskePromille = -onskePromille+1000;
+console.log(resultatValue);
+
+
